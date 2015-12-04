7983566850ba51be0f08efabd2ca1a6a42838ed6
NathiyaM <m.nathiyacs@gmail.com>
2015-12-04 00:36:45 +0000

    Sorting Program Added

--- DataStructures/InfixExpression/EvaluatePostfixExpression.c
+++ DataStructures/InfixExpression/EvaluatePostfixExpression.c
@@ -1,96 +1,101 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #define MAXCOLS 80
 #define TRUE 1
 #define FALSE 0
+double eval (char expr [] );
+double pop (struct stack *ps);
+void push (struct stack *ps, double x);
+int empty (struct stack *pq);
+int isdigit (char symb);
+double oper (int symb, double op1, double op2);
+
 struct stack {
 int top;
 double items [MAXCOLS];
 };
-double eval (char [] );
-double pop (struct stack *);
-void push (struct stack *, double);
-int empty (struct stack *);
-//int isdigit (int);
-double oper (int, double, double);
 
 void main ()
 {
-	char expr [MAXCOLS] = {"97+5-"} ;
-	int position = 0;
-	/* The class assignment may need to take
-	care of multi digit input.*/
-	printf ("%s%s", "the original postfixexpression is", expr);
-	printf ("\n %f", eval (expr) );
-	return;
+char expr [MAXCOLS] = {"97+5-"} ;
+int position = 0;
+/* The class assignment may need to take
+care of multi digit input.*/
+printf ("%s%s", "the original postfix expression is", expr);
+printf ("\n %f", eval (expr) );
+return;
 }
-/*int isdigit (int symb)
+
+int isdigit (char symb)
 {
-	return (symb >= 0 && symb <= 9);
-}	*/
+return (symb >= '0' && symb <= '9');
+}
 
 double eval (char expr [] )
 {
-	int c, position;
-	double opnd1, opnd2, value;
-	struct stack opndstk;
-	opndstk.top = -1;
-	for (position = 0; (c = expr [position] ) != '\0'; position++)
-	if (isdigit(c) ) /* is operand */
-		push (&opndstk, c );
-	/* it was push (&opndstk, (double (c - '0') ); before */
-	else /* is operator */
-	{
-		opnd2 = pop (&opndstk);
-		opnd1 = pop (&opndstk);
-		value = oper (c, opnd1, opnd2);
-		push (&opndstk, value);
-	} /* else operator */
+int c, position;
+double opnd1, opnd2, value;
+struct stack opndstk;
+opndstk.top = -1;
+for (position = 0; (c = expr [position] ) != '\0'; position++)
+if (isdigit(c) ) /* is operand */
+push (&opndstk, c );
+/* it was push (&opndstk, (double (c - '0') ); before */
+else /* is operator */
+{
+opnd2 = pop (&opndstk);
+opnd1 = pop (&opndstk);
+value = oper (c, opnd1, opnd2);
+push (&opndstk, value);
+} /* else operator */
 return (pop (&opndstk) );
 } /* end eval function */
 
 int empty (struct stack *pq)
 {
-	if (pq->top == MAXCOLS-1)
-		return (TRUE);
-	else
-		return (FALSE);
+if (pq->front == pq->rear)
+return (TRUE);
+else
+return (FALSE);
 } /* end of function empty */
 
 double oper (int symb, double op1, double op2)
 {
-	switch (symb)
-	{
-		case + : return (op1 + op2);
-		case - : return (op1 - op2);
-		case * : return (op1 * op2);
-		case / : return (op1 / op2);
-		case % : return ( (int) op1 % (int)op2 );
-		case ^ : return (pow( op1,op2) );
-		default : printf ("%s", "illegal operation");
-		exit (1);
-	} /* end of switch symb */
+switch (symb)
+{
+case '+' : return (op1 + op2);
+case '-' : return (op1 - op2);
+case '*' : return (op1 * op2);
+case '/' : return (op1 / op2);
+case '%' : return ( (int) op1 % (int)op2 );
+case '^' : return ( pow( op1, op2) );
+default : printf ("%s", "illegal operation");
+exit (1);
+} /* end of switch symb */
 } /* end of function oper */
+
 
 double pop (struct stack *ps)
 {
-	if (empty (ps) )
-	{
-		printf ("%s", "stack underflow");
-		exit (1);
-	} /* end of empty if */
+if (empty (ps) )
+{
+printf ("%s", "stack underflow");
+exit (1);
+} /* end of empty if */
 return (ps->items[ps->top--] );
 } /* end of pop function */
 
+
 void push (struct stack *ps, double x)
 {
-	if (ps->top == MAXCOLS-1)
-	{
-		printf ("%s", "stack overflow");
-		exit (1);
-	} /* end of if */
-	else
-		ps->items[++(ps->top) ] = x;
+if (ps->top == MAXCOLS-1)
+{
+printf ("%s", "stack overflow");
+exit (1);
+} /* end of if */
+else
+ps->items[++(ps->top) ] = x;
 return;
 } /* end of push function */
+
--- DataStructures/sorting/Sorting.c
+++ DataStructures/sorting/Sorting.c
@@ -1,10 +1,16 @@
-# include<stdio.h>
+#include<stdio.h>
 #include<stdlib.h>
 #define SIZE 40
 void bubblesort(int array[], int size);
 void insertsort(int array[], int size);
 void selectionsort(int array[], int size);
 void shellsort(int array[], int size);
+void quicksort(int x[],int first,int last);
+void mergeSort(int arr[],int low,int mid,int high);
+void partition(int arr[],int low,int high);
+void heapsort(int a[],int n);
+void heapify(int a[],int n);
+void adjust(int a[],int n);
 void printarray(int arraynum[], int size);
 void swap(int *array1, int *array2);
 int main(int argc, char *argv[])
@@ -38,45 +44,78 @@ and split the records as number, and store them in the array.*/
 		fclose(iptr);//closing of reading file
 	}
 	printf("Enter the choice in which method you want your array to be sorted\n");
-	printf("****SIMPLE SORT****\n1.BubbleSort \n2.InsertionSort \n3.SelectionSort\n4.ShellSort\n");
-	printf("enter your choice\n");
+	printf("1.Simple Sort 2.Advance Sort\n");
+    printf("enter your choice\n");
 	scanf("%d", &choice);
-	printf("The array before sorting is \n");
-	printarray(number, SIZE);
 	switch(choice)
 	{
- 	case 1:
-		bubblesort(number, SIZE);
-		break;
-	case 2:
-		insertsort(number, SIZE);
-		break;
-	case 3:
-		selectionsort(number, SIZE);
-		break;
-	case 4:
-		shellsort(number, SIZE);
-		break;
-	}
+	   case 1:
+	    printf("****SIMPLE SORT****\n1.BubbleSort \n2.InsertionSort \n3.SelectionSort\n4.ShellSort\n");
+	    printf("enter your choice\n");
+	    scanf("%d", &choice);
+	    printf("The array before sorting is \n");
+	    printarray(number, SIZE);
+    	switch(choice)
+    	{
+        	case 1:
+                bubblesort(number, SIZE);
+            	break;
+        	case 2:
+                insertsort(number, SIZE);
+                break;
+            case 3:
+                selectionsort(number, SIZE);
+                break;
+            case 4:
+                shellsort(number, SIZE);
+                break;
+    	}
+    	break;
+        case 2:
+            printf("*****Advanced Sort *****\n1.Quick Sort \n 2.Heap Sort \n 3.Merge Sort \n");
+            printf("enter your choice\n");
+	        scanf("%d", &choice);
+	        printf("The array before sorting is \n");
+	        printarray(number, SIZE);
+            switch(choice)
+    	    {
+    	        case 1:
+    	            quicksort(number,0,SIZE-1);
+    	            printf("The Array Elements  after sorting is \n");
+    	            printarray(number,SIZE);
+    	            break;
+    	       case 2:
+    	            heapsort(number,SIZE);
+    	            printf("The Array Elements  after sorting is \n");
+    	            printarray(number,SIZE);
+    	            break;
+    	       case 3:
+    	            partition(number,0,SIZE-1);
+    	            printf("The Array Elements  after sorting is \n");
+    	            printarray(number,SIZE);
+    	            break;
+    	            
+    	    }
+    	   break; 
+     }
 return 0;
 }	
 
+// Prints the element in the array
 void printarray(int arraynum[], int size)
 {
 	int i;
-	printf("The Array Elements  after sorting is \n");
 	for(i=0;i<size;i++)
 	{
 		printf("%d\t", arraynum[i]);
 	}
 }
 
-
+//Bubble Sort Implemetation
 void bubblesort(int array[], int size)
 {
 	int j, pass;
 	//sort the array by comparing the adjacent element and swap the array element if its larger
-	printf("The array after bubble sort is\n")
 	for(pass=0;pass<size-1;pass++)
 	{
 		for(j=0;j<size-1;j++)
@@ -90,6 +129,7 @@ void bubblesort(int array[], int size)
 	printarray(array,size);
 }
 
+//Insert Sort Implemetation
 void insertsort(int array[], int size)
 {
 	int j, i;
@@ -100,13 +140,12 @@ void insertsort(int array[], int size)
 		{
 			swap(&array[j], &array[j-1]);
 			j=j-1;
-			printf("The array sorted after each pass is \n");
 		}
 	}
 			printarray(array, size);
 }
 
-
+//selectionsort Implemetation
 void selectionsort(int array[], int size)
 {
 	int i, j;
@@ -130,7 +169,7 @@ void selectionsort(int array[], int size)
 	printarray(array, size);
 }
 
-
+//shellsort implementation
 void shellsort(int array[],int size)
 {
     int i = 0,  j = 0, k = 0, mid = 0;
@@ -155,7 +194,150 @@ void shellsort(int array[],int size)
     return ;
 }
 
+//Quick Sort Implementation
+void quicksort(int x[],int first,int last)
+{
+    int pivot,j,temp,i;
 
+     if(first<last)
+     {
+         pivot=first;
+         i=first;
+         j=last;
+
+         while(i<j)
+         {
+             while(x[i]<=x[pivot]&&i<last)
+                 i++;
+             while(x[j]>x[pivot])
+                 j--;
+             if(i<j)
+             {
+                temp=x[i];
+                x[i]=x[j];
+                 x[j]=temp;
+             }
+         }
+
+         temp=x[pivot];
+         x[pivot]=x[j];
+         x[j]=temp;
+         quicksort(x,first,j-1);
+         quicksort(x,j+1,last);
+
+    }
+    
+    return ;
+}
+
+
+void partition(int arr[],int low,int high)
+{
+    int mid;
+
+    if(low<high)
+    {
+         mid=(low+high)/2;
+         partition(arr,low,mid);
+         partition(arr,mid+1,high);
+         mergeSort(arr,low,mid,high);
+    }
+}
+
+void mergeSort(int arr[],int low,int mid,int high)
+
+{
+
+    int i,m,k,l,temp[SIZE];
+
+    l=low;
+    i=low;
+    m=mid+1;
+
+    while((l<=mid)&&(m<=high)){
+
+         if(arr[l]<=arr[m]){
+             temp[i]=arr[l];
+             l++;
+         }
+         else{
+             temp[i]=arr[m];
+             m++;
+         }
+         i++;
+    }
+
+    if(l>mid){
+         for(k=m;k<=high;k++){
+             temp[i]=arr[k];
+             i++;
+         }
+    }
+    else{
+         for(k=l;k<=mid;k++){
+             temp[i]=arr[k];
+             i++;
+         }
+    }
+   
+    for(k=low;k<=high;k++){
+         arr[k]=temp[k];
+    }
+}
+
+void heapsort(int a[],int n) 
+{
+	int i,t;
+	heapify(a,n);
+	for (i=n-1;i>0;i--) 
+	{
+		t = a[0];
+		a[0] = a[i];
+		a[i] = t;
+		adjust(a,i);
+	}
+}
+
+void heapify(int a[],int n) {
+	int k,i,j,item;
+	for (k=1;k<n;k++) 
+	{
+		item = a[k];
+		i = k;
+		j = (i-1)/2;
+		while((i>0)&&(item>a[j])) 
+		{
+			a[i] = a[j];
+			i = j;
+			j = (i-1)/2;
+		}
+		a[i] = item;
+	}
+}
+
+void adjust(int a[],int n)
+{
+	int i,j,item;
+	j = 0;
+	item = a[j];
+	i = 2*j+1;
+	while(i<=n-1) 
+	{
+		if(i+1 <= n-1)
+		   if(a[i] <a[i+1])
+		    i++;
+		if(item<a[i]) 
+		{
+			a[j] = a[i];
+			j = i;
+			i = 2*j+1;
+		} else
+		   break;
+	}
+	a[j] = item;
+}
+
+//Swap the data
 void swap(int *array1, int *array2)
 {
 	int tmp;
